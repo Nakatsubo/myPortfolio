c88e3e2bac5254d3d8b2fe9a798e33fa244231d2
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-03-02 23:27:05 +0900

    create global scss settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'ress',
+    '~/assets/scss/base.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -32,6 +34,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/style-resources',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -40,5 +43,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  // Customize Settings
+  styleResources: {
+    scss: ['~/assets/scss/app.scss'],
+    hoistUseStatements: true,
+  },
 }
