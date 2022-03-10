ddb3ed008bec184c36a3022fa82a63f0d3ef4fe3
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-03-10 22:57:38 +0900

    add global scss settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     'ress',
-    '~/assets/scss/base.scss'
+    '~/assets/scss/base.scss',
+    '~/assets/scss/global.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
