2817c5193caa57d1120d66c012ab2f9d8c94066f
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-03-17 20:21:33 +0900

    【Fixed】create Randam Background Position

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,3 +8,32 @@
     <TheFooter class="root__footer" />
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    setInterval(this.randambackgroundPosition, 100)
+  },
+  methods: {
+    randambackgroundPosition() {
+      const BG_SAND = document.querySelector('.bgSand')
+      let counter = Math.ceil(80 * Math.random())
+      BG_SAND.style.backgroundPosition = `${counter}% ${counter}%`;
+    }
+  } 
+}
+</script>
+
+<style lang="scss" scoped>
+.bgSand {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('/bg_sand.png');
+  background-repeat: repeat;
+  background-position: 50%;
+  opacity: 0.08;
+}
+</style>
